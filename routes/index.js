c017bc87cb55bec38f37ba15e314e19e8e40ed7b
raysebas24 <ray.sebastian100@gmail.com>
2022-03-01 09:27:19 +0100

    say hallo on /hallo

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get("/hallo", (reg, res) => {
+  res.send("Das ist ein Test!");
+})
+
 module.exports = router;
